ea3ff3a76f8949e7a7dc0508fdbef72505eb74d9
Ed Bartosh <eduard.bartosh@intel.com>
2021-02-02 12:17:30 +0200

    get rid of unused coverage module

--- cmd/cdi-driver/main_test.go
+++ cmd/cdi-driver/main_test.go
@@ -4,9 +4,8 @@ import (
 	"testing"
 
 	"github.com/intel/cdi/pkg/cdi-driver"
-	"github.com/intel/cdi/pkg/coverage"
 )
 
 func TestMain(t *testing.T) {
-	coverage.Run(cdidriver.Main)
+	cdidriver.Main()
 }
